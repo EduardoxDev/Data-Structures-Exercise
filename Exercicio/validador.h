cec0b66d8a58913ae3039b205eb40af44e1bbff6
Eduardo Maciel <duduwanka@gmail.com>
2026-03-03 21:37:21 -0300

    Add validation methods for age and residence

--- Exercicio/validador.h
+++ Exercicio/validador.h
@@ -7,6 +7,8 @@ class Validador {
 public:
     static bool validarRA(int ra);
     static bool validarNome(const std::string& nome);
+    static bool validarIdade(int idade);
+    static bool validarMoradia(const std::string& moradia);
     static std::string obterMensagemErro();
 private:
     static std::string mensagemErro;

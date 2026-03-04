eb033a313b056e5ebff9d8d19cdbbb9872359d61
Eduardo Maciel <duduwanka@gmail.com>
2026-03-03 21:37:08 -0300

    Add age and residence validation methods

--- Exercicio/validador.cpp
+++ Exercicio/validador.cpp
@@ -30,6 +30,34 @@ bool Validador::validarNome(const std::string& nome) {
     return true;
 }
 
+bool Validador::validarIdade(int idade) {
+    if (idade < 0) {
+        mensagemErro = "Idade nao pode ser negativa";
+        return false;
+    }
+    if (idade > 100) {
+        mensagemErro = "Idade deve ser menor que 100";
+        return false;
+    }
+    return true;
+}
+
 std::string Validador::obterMensagemErro() {
     return mensagemErro;
+}
+
+bool Validador::validarMoradia(const std::string& moradia) {
+    if (moradia.empty()) {
+        mensagemErro = "Moradia nao pode estar vazia";
+        return false;
+    }
+    if (moradia.length() < 3) {
+        mensagemErro = "Moradia deve ter pelo menos 3 caracteres";
+        return false;
+    }
+    if (moradia.length() > 100) {
+        mensagemErro = "Moradia deve ter no maximo 100 caracteres";
+        return false;
+    }
+    return true;
 }

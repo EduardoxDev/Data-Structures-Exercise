d7afa4edcd7b699eb7a1f7ccb9f850146ae32930
Eduardo Maciel <duduwanka@gmail.com>
2026-03-03 21:37:33 -0300

    Refactor hash.cpp to improve clarity and structure
    Refactor hash table implementation and remove unused methods.

--- Exercicio/hash.cpp
+++ Exercicio/hash.cpp
@@ -10,7 +10,6 @@ int funcaoHash(int chave, int max_posicoes) {
 Hash::Hash(int t) {
     max_itens = 0;
     max_posicoes = t;
-    colisoes = 0;
     estrutura = new Aluno[max_posicoes];
 }
 
@@ -18,110 +17,24 @@ Hash::~Hash() {
     delete[] estrutura;
 }
 
-bool Hash::estacheio() const {
+bool Hash::estacheio() {
     return max_itens == max_posicoes;
 }
 
-int Hash::obterTamanhoAtual() const {
+int Hash::obterTamanhoAtual() {
     return max_itens;
-}
-
-int Hash::obterColisoes() const {
-    return colisoes;
-}
-
-int Hash::sondagemLinear(int ra) {
-    int posicao = funcaoHash(ra, max_posicoes);
-    int inicio = posicao;
-    
-    while (estrutura[posicao].obterRa() != 0 && estrutura[posicao].obterRa() != ra) {
-        posicao = (posicao + 1) % max_posicoes;
-        if (posicao == inicio) return -1;
-        colisoes++;
-    }
-    return posicao;
 }
 
 void Hash::inserir(Aluno a) {
     if (estacheio()) {
-        return;
-    }
-    
-    int posicao = sondagemLinear(a.obterRa());
-    if (posicao != -1 && estrutura[posicao].obterRa() == 0) {
-        estrutura[posicao] = a;
-        max_itens++;
+        
     }
 }
 
-bool Hash::buscar(int ra, Aluno& resultado) {
-    int posicao = funcaoHash(ra, max_posicoes);
-    int inicio = posicao;
-    
-    while (estrutura[posicao].obterRa() != 0) {
-        if (estrutura[posicao].obterRa() == ra && estrutura[posicao].estaAtivo()) {
-            resultado = estrutura[posicao];
-            return true;
-        }
-        posicao = (posicao + 1) % max_posicoes;
-        if (posicao == inicio) break;
-    }
-    return false;
-}
-
-bool Hash::remover(int ra) {
-    int posicao = funcaoHash(ra, max_posicoes);
-    int inicio = posicao;
-    
-    while (estrutura[posicao].obterRa() != 0) {
-        if (estrutura[posicao].obterRa() == ra && estrutura[posicao].estaAtivo()) {
-            estrutura[posicao].desativar();
-            max_itens--;
-            return true;
-        }
-        posicao = (posicao + 1) % max_posicoes;
-        if (posicao == inicio) break;
-    }
-    return false;
-}
-
-void Hash::imprimir() const {
-    cout << "\n+" << string(60, '-') << "+" << endl;
-    cout << "| " << "TABELA HASH - ALUNOS CADASTRADOS" << string(27, ' ') << "|" << endl;
-    cout << "+" << string(60, '-') << "+" << endl;
-    
-    bool encontrou = false;
+void Hash::imprimir() {
     for (int i = 0; i < max_posicoes; i++) {
-        if (estrutura[i].obterRa() != 0 && estrutura[i].estaAtivo()) {
-            cout << "| Pos: " << i << " | RA: " << estrutura[i].obterRa() 
-                 << " | Nome: " << estrutura[i].obterNome() << endl;
-            encontrou = true;
+        if (estrutura[i].obterRa() != 0) {
+            cout << "RA: " << estrutura[i].obterRa() << ", Nome: " << estrutura[i].obterNome() << endl;
         }
     }
-    
-    if (!encontrou) {
-        cout << "| " << "Nenhum aluno cadastrado" << string(36, ' ') << "|" << endl;
-    }
-    cout << "+" << string(60, '-') << "+" << endl;
-}
-
-void Hash::imprimirEstatisticas() const {
-    float fatorCarga = (float)max_itens / max_posicoes * 100;
-    
-    cout << "\n+" << string(50, '=') << "+" << endl;
-    cout << "| " << "ESTATISTICAS DA TABELA HASH" << string(22, ' ') << "|" << endl;
-    cout << "+" << string(50, '=') << "+" << endl;
-    cout << "| Capacidade Total: " << max_posicoes << string(30 - to_string(max_posicoes).length(), ' ') << "|" << endl;
-    cout << "| Itens Armazenados: " << max_itens << string(29 - to_string(max_itens).length(), ' ') << "|" << endl;
-    cout << "| Espacos Livres: " << (max_posicoes - max_itens) << string(32 - to_string(max_posicoes - max_itens).length(), ' ') << "|" << endl;
-    cout << "| Colisoes Totais: " << colisoes << string(31 - to_string(colisoes).length(), ' ') << "|" << endl;
-    cout << "| Fator de Carga: " << fatorCarga << "%" << string(29 - to_string((int)fatorCarga).length(), ' ') << "|" << endl;
-    cout << "+" << string(50, '=') << "+" << endl;
-}
-
-void Hash::limpar() {
-    delete[] estrutura;
-    estrutura = new Aluno[max_posicoes];
-    max_itens = 0;
-    colisoes = 0;
 }
